611db4b4dcc5443397b75883c2d4bc6f7633bb2b
Iain Scott <odyssey3007@hotmail.com>
2016-03-25 13:57:24 +0000

    go!

--- PUSH1-master/Customerinfo.php
+++ PUSH1-master/Customerinfo.php
@@ -208,10 +208,10 @@ try{
         </div>
 
         <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-            <span class="sr-only"><img src="../../untitledcarousel/img/previous3.png"></span>
+            <span class="sr-only"><img src="assets/previous3.png"></span>
         </a>
         <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-            <span class="sr-only"><img src="img/next3.png"></span>
+            <span class="sr-only"><img src="assets/next3.png"></span>
         </a>
     </div>
 
