f564e060a7a6d4f80fdb118db0f2f959afa3bb14
Iain Scott <odyssey3007@hotmail.com>
2016-03-25 20:30:48 +0000

    go!

--- PUSH1-master/send.php
+++ PUSH1-master/send.php
@@ -5,6 +5,7 @@ $firstname = $_POST['firstname'];
 $surname = $_POST['surname'];
 $email = $_POST['email'];
 $telephone = $_POST['telephone'];
+$roomname = $_POST['roomname'];
 
 require 'PHPMailerAutoload.php';
 $mail = new PHPMailer;
@@ -16,9 +17,10 @@ $mail->Username = "thebnbhub@outlook.com";
 $mail->Password = "Pedro123";
 $mail->setFrom('thebnbhub@outlook.com');
 $mail->addAddress($email);
+$mail->addCCAddress($email);
 $mail->Subject = 'Booking Confirmation';
 //$mail->msgHTML(file_get_contents('contents.html'), dirname(testpro));
-$mail->Body = 'Name: '.$title.' '.$firstname.' '.$surname."\n".'Your Email: '.$email."\n".'Your telephone: '.$telephone;
+$mail->Body = 'Room Name :'.$roomname."\n".'Name: '.$title.' '.$firstname.' '.$surname."\n".'Your Email: '.$email."\n".'Your telephone: '.$telephone;
 //$mail->addAttachment('');
 
 if (!$mail->send()) {
@@ -26,6 +28,8 @@ if (!$mail->send()) {
 } else {
     echo "Welcome to the Booking Confirmation Page!!!<p>";
     echo "A confirmation email has been sent!<p>";
+    echo "Here are your Booking details...<p>";
+    echo "Room Name :".$roomname."\n"."Name: ".$title." ".$firstname." ".$surname."\n"."Your Email: ".$email."\n"."Your telephone: ".$telephone;
     echo "<a href='customerinfo.php'>Return to the Booking Page</a>";
 }
 
