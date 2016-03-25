5a09cbbf70afc1ca8257f8d61535ba19b29fce24
Iain Scott <odyssey3007@hotmail.com>
2016-03-25 23:39:08 +0000

    go!

--- PUSH1-master/SearchBB.php
+++ PUSH1-master/SearchBB.php
@@ -298,13 +298,11 @@ NEWHTML;
 
         <table border="0" class="tablejava">
             <tr>
-                <td>Aberdeen
-
+                <td>
                     <form action="searchResultsPage.php" method="POST">
                         <input hidden name="location" value='aberdeen'>
                         <button class="btnclear" type="submit" >Aberdeen</button>
                     </form>
-
                 </td>
                 <td>Edinburgh</td>
                 <td>London</td>
