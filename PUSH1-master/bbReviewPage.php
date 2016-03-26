7547ccd478d0b438b427b0dfee9ff074bbba98c4
Iain Scott <odyssey3007@hotmail.com>
2016-03-26 02:24:14 +0000

    go!

--- PUSH1-master/bbReviewPage.php
+++ PUSH1-master/bbReviewPage.php
@@ -128,7 +128,7 @@ session_start();
         $conn = new PDO ( "sqlsrv:server = tcp:bbsqldb.database.windows.net,1433; Database = SQL_BB", "teamdsqldb", "Sql20022016*");
         $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
         try{
-            $st = $conn-> query("SELECT * FROM [B&B] WHERE [email] = '$email'");
+            $st = $conn-> query("SELECT * FROM [B&B] WHERE [bbemail] = '$email'");
             foreach($st->fetchAll() as $row) {
                 $newhtml =
                     <<<NEWHTML
