5215f01009dc7e03a13a593d7a305f9b9a91c9b5
jd90 <1418146@rgu.ac.uk>
2016-03-27 10:40:52 +0100

    iiiii

--- PUSH1-master/Customerinfo.php
+++ PUSH1-master/Customerinfo.php
@@ -166,7 +166,7 @@ catch(PDOException $e)
 
 
 <?php
-$bbid = $_POST['bbid'];
+$bbid = $_GET['bbid'];
 $conn = new PDO ( "sqlsrv:server = tcp:bbsqldb.database.windows.net,1433; Database = SQL_BB", "teamdsqldb", "Sql20022016*");
 $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 try{
