499e715cd9461498e3b8366397c77fdaafd7ec9b
jd90 <1418146@rgu.ac.uk>
2016-03-27 17:06:56 +0100

    iiiii

--- PUSH1-master/SearchBB.php
+++ PUSH1-master/SearchBB.php
@@ -14,6 +14,7 @@ session_start();
           href="assets/b&bicon.png">
     <link type="text/css" rel="stylesheet" href="style.css"/>
     <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
     <title>Search: theB&Bhub</title>
 
 
